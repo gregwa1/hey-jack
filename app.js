9cd994fba75cd82912158c148025ddc8a0a35fc0
grewa1 <gbjrmail@gmail.com>
2019-10-01 17:30:39 -0400

    added css. having issue with java

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const player = '';
-const computer = '';
+const player = ``;
+const dealer = '';
+const display = document.querySelector("#display");
+
+// Greeting the player
+const greet = () => {
+  let name = getInput("Welcome! Let's play a game. What's your name?");
+  console.log(name);
+  return name;
+}
 
 //Shuffling Deck
 let shuffleDeck = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
@@ -11,7 +19,7 @@ let stayButton = document.querySelector('#stay');
 
 //dealing cards
 let dealCards = async (cards) => {
-  let response = await axios.get(`https://deckofcardsapi.com/api/deck/${cards}/draw/?count=2`);
+  let response = await axios.get(`https://deckofcardsapi.com/api/deck/${cards}/draw/?count=4`);
   // console.log(response.data.cards);
   return response.data.cards;
 }
@@ -19,7 +27,47 @@ let dealCards = async (cards) => {
 //Getting deck_id
 playButton.addEventListener(`click`, async () => {
   let response = await axios.get(`https://deckofcardsapi.com/api/deck/o9z1x6oiqd0i/shuffle/?deck_count=1`);
-  console.log(response.data.deck_id)
-  let twoCards = await dealCards(response.data.deck_id)
-  console.log(twoCards)
+  console.log(response.data.deck_id);
+  let twoCards = await dealCards(response.data.deck_id);
+  console.log(twoCards);
 });
+
+//Each player gets two cards. position, value, and image
+let players = (cards) => {
+  for (let i = 0; i < twoCards.length; i++) {
+    if (i === 0) {
+      player.style.div = `https://deckofcardsapi.com/api/deck/${cards}/draw/?count=4(${cards[i].image})`
+    } else if (i === 1) {
+
+    }
+
+  }
+  cards.forEach((cards) => {
+    let cardDiv = document.createElement("#div");
+    cardDiv.style.backgroundImage = `url(${card.image})`
+    display.append(cardDiv);
+  })
+}
+
+
+// from Tony
+// const showCards = (cards) => {
+//   // cards.forEach((card) => {
+//   for (let i = 0; i < cards.length; i++) {
+//     if (i === 0) {
+//       playArea1.style.backgroundImage = `url(${cards[i].image})`
+//     } else {
+//       playArea2.style.backgroundImage = `url(${cards[i].image})`
+//     }
+//   }
+//   // });
+// }
+// let renderList = (cards) => {
+//   display.innerHTML = "";
+//   cards.forEach((card) => {
+//     const cardOutput = document.createElement('#playerCard1');
+//     cardOutput.innerHTML = `<img src=${card.image} />`;
+//     // movieList.append(movieOutput);
+//     display.append(cardOutput);
+//   })
+// }
