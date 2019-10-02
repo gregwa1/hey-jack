22e08d2b84b38e3fd14cda7b71a819469c5ca193
grewa1 <gbjrmail@gmail.com>
2019-10-02 15:39:38 -0400

    four cards displayed. winner displayed on first draw

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
 const displayPlayer = document.querySelector("#display-player");
 const displayDealer = document.querySelector("#display-dealer");
-let url = ``
+const results = document.querySelector("#results");
+let url = ``;
 let shuffleDeck;
+var playerCard1;
+var playerCard2;
+var dealerCard1;
+var dealerCard2;
 
 // Greeting the player
 const greet = () => {
@@ -25,25 +30,86 @@ let stayButton = document.querySelector('#stay');
 //dealing cards
 let dealCards = async (cards) => {
   let response = await axios.get(`https://deckofcardsapi.com/api/deck/${shuffleDeck}/draw/?count=4`);
-
   players(response.data.cards);
+  titlePlayer.style = `display: block`;
+  titleDealer.style = `display: block`;
 }
 
 //Getting deck_id
-playButton.addEventListener(`click`, dealCards);
+let titlePlayer = document.querySelector("#headingPlayer");
+let titleDealer = document.querySelector("#headingDealer");
+
+playButton.addEventListener(`click`, dealCards)
+
+let changeRank = (value) => {
+  if (value === "JACK" || value === "QUEEN" || value === "KING") {
+    return 10;
+  } else if (value === "ACE") {
+    return 11;
+  } else {
+    return parseInt(value);
+  }
+}
+
 
 //Each player gets two cards. position, value, and image
-let players = (cards) => {
+const players = (cards) => {
+
   for (let i = 0; i < cards.length; i++) {
-    if (i === 0 || i === 2) {
+    if (i === 0) {
       const playerCardDiv = document.createElement('div');
       playerCardDiv.innerHTML = `<img src=${cards[i].image} />`;
       displayPlayer.append(playerCardDiv);
-    } else if (i === 1 || i === 3) {
+      playerCard1 = changeRank(cards[i].value)
+    }
+    else if (i === 2) {
+      const playerCardDiv = document.createElement('div');
+      playerCardDiv.innerHTML = `<img src=${cards[i].image} />`;
+      displayPlayer.append(playerCardDiv);
+      playerCard2 = changeRank(cards[i].value)
+    } else if (i === 1) {
       const dealerCardDiv = document.createElement('div');
       dealerCardDiv.innerHTML = `<img src=${cards[i].image} />`;
       displayDealer.append(dealerCardDiv);
+      dealerCard1 = changeRank(cards[i].value)
+    } else if (i === 3) {
+      const dealerCardDiv = document.createElement('div');
+      dealerCardDiv.innerHTML = `<img src=${cards[i].image} />`;
+      displayDealer.append(dealerCardDiv);
+      dealerCard2 = changeRank(cards[i].value)
     }
 
   }
+  //Adding cards after the delt.
+  console.log(playerCard1, playerCard2, dealerCard1, dealerCard2);
+  let playerScore = getTotal(playerCard1, playerCard2);
+  let dealerScore = getTotal(dealerCard1, dealerCard2);
+  if (playerScore === 21 && playerScore === dealerScore) {
+    let winner = document.createElement('div');
+    winner.innerHTML = `<h2>Player and dealer tie at 21. Dealer wins!</h2>`
+    results.append(winner);
+  } else if (playerScore > dealerScore) {
+    let winner = document.createElement('div')
+    winner.innerHTML = `<h2>Player wins with score of ${playerScore}</h2>`
+    results.append(winner);
+  } else if (dealerScore > playerScore) {
+    let winner = document.createElement('div')
+    winner.innerHTML = `<h2>Dealer wins with score of ${dealerScore}</h2>`
+    results.append(winner);
+  }
+
+
+}
+
+getTotal = (card1, card2) => {
+  return card1 + card2;
+}
+//count cards and see if cards add up to 21 or bust.
+const determineWinner = (player1, player2) => {
+  let score = 0;
+  for (let i = 0; i < array.length; i++) {
+    let cardValue = array[i].value;
+    score += cardValue;
+  }
+  return score
 }
